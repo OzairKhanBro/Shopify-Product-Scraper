ac475fa1919e10971460c2cdcd97446718d77a67
uzair Khan <uzairkhan456@gmail.com>
2022-07-01 20:31:31 +0500

    all columns required for product import added in csv file

--- shopify.py
+++ shopify.py
@@ -40,7 +40,7 @@ def get_page_collections(url):
     page = 1
     while True:
         req = urllib.request.Request(
-            full_url + '?page={}'.format(page),
+            full_url + '?limit=50&page={}'.format(page),
             data=None,
             headers={
                 'User-Agent': USER_AGENT
@@ -100,12 +100,7 @@ def extract_products_collection(url, col):
                 return ''
 
             for i, variant in enumerate(product['variants']):
-                price = variant['price']
-                option1_value = variant['option1'] or ''
-                option2_value = variant['option2'] or ''
-                option3_value = variant['option3'] or ''
-                option_value = ' '.join([option1_value, option2_value,
-                                         option3_value]).strip()
+                
                 sku = variant['sku']
                 main_image_src = ''
                 if product['images']:
@@ -115,14 +110,32 @@ def extract_products_collection(url, col):
                 stock = 'Yes'
                 if not variant['available']:
                     stock = 'No'
+                row = variant
+                row.update(product)
 
-                row = {'sku': sku, 'product_type': product_type,
-                       'title': title, 'option_value': option_value,
-                       'price': price, 'stock': stock, 'body': str(product['body_html']),
-                       'variant_id': product_handle + str(variant['id']),
-                       'product_url': product_url, 'image_src': image_src}
+                rowProps = {'sku': sku, 
+                        'product_type': product['product_type'],
+                        'title' : product['title'],
+                        'title' : product['title'],
+                        'product_type' : product['product_type'],
+                        'product_url' : url + '/products/' + product['handle'],
+                        'product_handle' : product['handle'],
+                        'option1_value' : variant['option1'] or '',
+                        'price' : variant['price'],
+                        'compare_at_price' : variant['compare_at_price'],
+                        'option2_value' : variant['option2'] or '',
+                        'option3_value' : variant['option3'] or '',
+                        'grams' : variant['grams'] or '',
+                        'requires_shipping' : variant['requires_shipping'] or 'TRUE',
+                        'stock': stock,
+                        # 'body': str(product['body_html']),
+                        'variant_id': product_handle + str(variant['id']),
+                        'product_url': product_url,
+                        'image_src': image_src
+                        }
+                row.update(rowProps)
                 for k in row:
-                    row[k] = str(row[k].strip()) if row[k] else ''
+                    row[k] = str(str(row[k]).strip()) if row[k] else ''
                 yield row
 
         page += 1
@@ -130,30 +143,101 @@ def extract_products_collection(url, col):
 
 
 def extract_products(url, path, collections=None):
-    with open(path, 'w') as f:
+    import os
+    if os.path.exists(path):
+        os.remove(path)
+    with open(path, 'w', encoding="utf-8", newline='') as f:
         writer = csv.writer(f)
-        writer.writerow(['Code', 'Collection', 'Category',
-                         'Name', 'Variant Name',
-                         'Price', 'In Stock', 'URL', 'Image URL', 'Body'])
+        writer.writerow(['Handle',	'Title',	'Body (HTML)',	'Vendor',	'Standardized Product Type',	'Custom Product Type',	'Tags',	'Published',	'Option1 Name',	'Option1 Value',	'Option2 Name',	'Option2 Value',	'Option3 Name',	'Option3 Value',	'Variant SKU',	'Variant Grams',	'Variant Inventory Tracker',	'Variant Inventory Qty',	'Variant Inventory Policy',	'Variant Fulfillment Service',	'Variant Price',	'Variant Compare At Price',	'Variant Requires Shipping',	'Variant Taxable',	'Variant Barcode',	'Image Src',	'Image Position',	'Image Alt Text',	'Gift Card',	'SEO Title',	'SEO Description',	'Google Shopping / Google Product Category',	'Google Shopping / Gender',	'Google Shopping / Age Group',	'Google Shopping / MPN',	'Google Shopping / AdWords Grouping',	'Google Shopping / AdWords Labels',	'Google Shopping / Condition',	'Google Shopping / Custom Product',	'Google Shopping / Custom Label 0',	'Google Shopping / Custom Label 1',	'Google Shopping / Custom Label 2',	'Google Shopping / Custom Label 3',	'Google Shopping / Custom Label 4',	'Variant Image',	'Variant Weight Unit',	'Variant Tax Code',	'Cost per item',	'Price / International',	'Compare At Price / International',	'Status'])
         seen_variants = set()
+        products =[]
         for col in get_page_collections(url):
             if collections and col['handle'] not in collections:
                 continue
-            handle = col['handle']
-            title = col['title']
-            for product in extract_products_collection(url, handle):
+            col_handle = col['handle']
+            co_title = col['title']
+            print('fetching product ' + col_handle)
+            for product in extract_products_collection(url, col_handle):
                 variant_id = product['variant_id']
                 if variant_id in seen_variants:
                     continue
-
                 seen_variants.add(variant_id)
-                writer.writerow([product['sku'], str(title),
-                                 product['product_type'],
-                                 product['title'], product['option_value'],
-                                 product['price'],
-                                 product['stock'], product['product_url'],
-                                 product['image_src'], product['body']])
+                option_name = ''
+                tags = ''
+                try:
+                    product['options']= json.loads(product['options'].replace("\'", "\""))
+                    option_name = product['options'][0]['name']
+                    # print(type(tags))
+                    # print(tags)
+                except Exception as e:
+                    print('error in options string: ' + product['options'])
+                    print(e)
+                    option_name='color'
+                    pass
+                try:
+                    tags = ','.join(json.loads(product['tags'].replace("\'", "\"")))
+                    # print('tags: '+tags)
+                except Exception as e:
 
+                    print(e)
+ 
+ 
+                # print(product['featured_image'].replace("\'", "\""))
+                # product['featured_image']= json.loads(product['featured_image'].replace("\'", "\""))
+                del product['body_html']
+                writer.writerow([
+                    product['product_handle'],
+                    product['title'],
+                    '',#Body html
+                    product['vendor'] or '', #Vendor
+                    '',#'Standardized Product Type',
+                    product['product_type'],#	'Custom Product Type',
+                    tags,# Tags
+                    'TRUE',	# 'Published',
+                    option_name,#	'Option1 Name',
+                    product['option1_value'],#	'Option1 Value',	
+                    '', #option2 Name
+                    product['option2_value'],#	'Option2 Value',	
+                    '', #option3 Name
+                    product['option3_value'],#	'Option3 Value',	
+                    product['sku'],    #'Variant SKU'	,
+                    product['grams'],#  'Variant Grams'	,
+                    '',    #    'Variant Inventory Tracker'	,
+                    '100',    #    'Variant Inventory Qty'	,
+                    'deny',    #    'Variant Inventory Policy'	,
+                    'manual',    # 'Variant Fulfillment Service'	,
+                     product['price'],   #  'Variant Price'	,
+                     product['compare_at_price'],   #    'Variant Compare At Price'	,
+                     product['requires_shipping'],   # 'Variant Requires Shipping'	,
+                     'FALSE',#product['taxable'],   #    'Variant Taxable'	,
+                     '',   #  'Variant Barcode'	,
+                       product['image_src'], #  'Image Src'	,
+                    '',#   product['featured_image']['position'],  #    'Image Position'	,
+                    '',#   product['featured_image']['alt'],  #   'Image Alt Text'	,
+                      'FALSE',  #   'Gift Card'	,
+                     '',   #    'SEO Title'	,
+                     '',   #    'SEO Description'	,
+                     col_handle,   #  'Google Shopping / Google Product Category'	,
+                      '',  #    'Google Shopping / Gender'	,
+                     '',   # 'Google Shopping / Age Group'	,
+                     '',   #  'Google Shopping / MPN'	,
+                      '',  #    'Google Shopping / AdWords Grouping'	,
+                     '',   #   'Google Shopping / AdWords Labels'	,
+                      '',  # 'Google Shopping / Condition'	,
+                      '',  #  'Google Shopping / Custom Product'	,
+                      '',  # 'Google Shopping / Custom Label 0'	,
+                      '',  # 'Google Shopping / Custom Label 1'	,
+                      '',  # 'Google Shopping / Custom Label 2'	,
+                      '',  # 'Google Shopping / Custom Label 3'	,
+                       '', # 'Google Shopping / Custom Label 4'	,
+                      '',  # 'Variant Image'	,
+                      '',  #    'Variant Weight Unit'	,
+                     '',   #  'Variant Tax Code'	,
+                      '',  # 'Cost per item'	,
+                      '',  #    'Price / International'	,
+                      '',  #    'Compare At Price / International'	,
+                      'ACTIVE'# 'Status'	,
+                ])
 
 if __name__ == '__main__':
     parser = OptionParser()
